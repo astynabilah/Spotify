c1f1042762571be224ed28069129467d0d2c4af9
@astynabilah <nabilahasty6@gmail.com>
2019-04-05 20:27:16 +0700

    test commit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,4 +10,18 @@ class Login extends CI_Controller
 
 	}
 
+	public function log_in(){
+		$this->db->where('username',$this->input->post('username'));
+		$this->db->where('password',sha1($this->input->post('password')));
+		$res = $this->db->get('users')->row_array();
+		if ($res['username']!=null){
+			$this->session->set_userdata('username',$res['username']);
+			$this->session->set_userdata('password',$res['password']);
+			redirect('login');
+		} else {
+			$this->session->set_flashdata('info','Wrong username/password');
+			redirect('login');
+		}
+	}
+
 }
--- application/views/template/home.php
+++ application/views/template/home.php
@@ -6,18 +6,14 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<title>Music for everyone - Spotify</title>
 <!------ Include the above in your HEAD tag ---------->
 </head>
-<body>
+<body style="background-image: linear-gradient(#3D3C4F, #3D3C4F);">
 <div class="container" style="margin-top: 20px;">
             <div class="row blog">
                 <div class="col-md-12">
                     <div id="blogCarousel" class="carousel slide" data-ride="carousel">
-
-                        <ol class="carousel-indicators">
-                            <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#blogCarousel" data-slide-to="1"></li>
-                        </ol>
 
                         <!-- Carousel items -->
                         <div class="carousel-inner">
@@ -26,22 +22,22 @@
                                 <div class="row">
                                     <div class="col-md-3">
                                         <a href="#">
-                                            <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;"><center>Top 50 Indonesia</center>
+                                            <img src="assets/genre/pop.PNG" alt="Image" style="max-width:100%;"><center>Pop</center>
                                         </a>
                                     </div>
                                     <div class="col-md-3">
                                         <a href="#">
-                                            <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;"><center>Top 50 Global</center>
+                                            <img src="assets/genre/jazz.PNG" alt="Image" style="max-width:100%;"><center>Jazz</center>
                                         </a>
                                     </div>
                                     <div class="col-md-3">
                                         <a href="#">
-                                            <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;"><center>Viral 50 Global</center>
+                                            <img src="assets/genre/rock.PNG" alt="Image" style="max-width:100%;"><center>Rock</center>
                                         </a>
                                     </div>
                                     <div class="col-md-3">
                                         <a href="#">
-                                            <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;"><center>Viral 50 Indonesia</center>
+                                            <img src="assets/genre/electronic.PNG" alt="Image" style="max-width:100%;"><center>Electronic</center>
                                         </a>
                                     </div>
                                 </div>
