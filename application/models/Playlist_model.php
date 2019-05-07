0970295e68fa38a76f781a39535300df1449161c
Deffri Indarta <deffri.indarta98@gmail.com>
2019-05-07 12:20:31 +0700

    ye

--- application/models/Playlist_model.php
+++ application/models/Playlist_model.php
@@ -2,15 +2,6 @@
 class Playlist_model extends CI_model
 {
 
-public function tambahPlaylist()
-	{
-		$data = [
-			"nama" => $this->input->post('nama', true),
-			"nim" => $this->input->post('nim', true),
-			"email" => $this->input->post('email', true),
-			"jurusan" => $this->input->post('jurusan', true),
-		];
 
-		return $this->db->insert('mahasiswa',$data);
-	}
+		
 }
